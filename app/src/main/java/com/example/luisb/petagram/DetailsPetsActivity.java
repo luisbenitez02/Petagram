76ec91b6716a029d92d26039c398e90900a76277
Luis Benitez <luisbenitez02@gmail.com>
2017-06-26 17:54:48 -0500

    Nuevas activities
    se crean nuevas actividades (en blanco)

---
+++ app/src/main/java/com/example/luisb/petagram/DetailsPetsActivity.java
@@ -0,0 +1,19 @@
+package com.example.luisb.petagram;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by luisb on 26/06/2017.
+ */
+
+public class DetailsPetsActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_details_pets);
+    }
+}
---
+++ app/src/main/java/com/example/luisb/petagram/ListPetsActivity.java
@@ -0,0 +1,13 @@
+package com.example.luisb.petagram;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListPetsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_pets);
+    }
+}
