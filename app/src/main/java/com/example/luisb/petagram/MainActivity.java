4bdfa91f360c7fb750a454f6853db29ca0b3ed17
Luis Benitez <luisbenitez02@gmail.com>
2017-06-26 21:55:57 -0500

    Boton Flotante
    Agregamos un boton flotante. Agregamos un icono que obtuvimos de internet y agragamos la libreria de compatibilidad 'com.android.support:design:25.3.1' en el gradle para lograr obtener el botoncito

--- app/src/main/java/com/example/luisb/petagram/MainActivity.java
+++ app/src/main/java/com/example/luisb/petagram/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.luisb.petagram;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        agregarFav();//llamo el metodo del botoncito flotante
+    }
+
+    public void agregarFav(){
+        FloatingActionButton miFav = (FloatingActionButton) findViewById(R.id.Myfav);
+
+        //vamos a crear una accion al tocarlo
+        miFav.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
+
