2e7bddbbca711833ec393b39e874532ea646d664
Luis Benitez <luisbenitez02@gmail.com>
2017-06-28 17:58:59 -0500

    Snackbar y deslizar para quitar
    Creamos un snackbar! el reemplazo de los antiguos Toast. Podemos ponerle acciones con un boton a la derecha y ademas creamos un CoordinatorLayout que contiene todos los elementos de nuestro layout y permite deslizar hacia la derecha y borrar la notificaicon

--- app/src/main/java/com/example/luisb/petagram/MainActivity.java
+++ app/src/main/java/com/example/luisb/petagram/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.luisb.petagram;
 
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,7 +24,18 @@ public class MainActivity extends AppCompatActivity {
         miFav.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Aqui escribe lo que quieres que se haga al presionar el boton
+                //Toast.makeText(getBaseContext(), getResources().getString(R.string.mensaje), Toast.LENGTH_SHORT).show();
 
+                //LENGTH_SHORT = duracion corta
+                //LENGTH_LONG = duracion larga
+
+                Snackbar.make(v,getResources().getString(R.string.mensaje), Snackbar.LENGTH_LONG).setAction(getResources().getString(R.string.text_action), new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Log.i("SNACKBAR","Clik en Snackbar");
+                    }
+                }).setActionTextColor(getResources().getColor(R.color.colorPrimary)).show();
             }
         });
     }
