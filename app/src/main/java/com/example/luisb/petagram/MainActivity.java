620bf40749673e8f53d5663d05e7dcfa72e18a74
Luis Benitez <luisbenitez02@gmail.com>
2017-06-28 20:50:08 -0500

    Creando una bolita de refresh
    Has visto los circulitos de chrome para recargar la pagina o en outlook o en instagram? Bueno eso hicimos.
    
    Debemos crear un elemento en el .XML <android.support.v4.widget.SwipeRefreshLayout> y dentro de el poner lo que queremos se refresque, en este caso un ListView.
    
    En neustro archivo .java instaciamos los elementos de la vista y creamos los metodos necesarios para que funcione

--- app/src/main/java/com/example/luisb/petagram/MainActivity.java
+++ app/src/main/java/com/example/luisb/petagram/MainActivity.java
@@ -2,19 +2,57 @@ package com.example.luisb.petagram;
 
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    SwipeRefreshLayout sfiMiIndicadorRefresh;
+    ListView lstMiLista;
+    Adapter adaptador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         agregarFav();//llamo el metodo del botoncito flotante
+
+        //creamos el objeto (lo referenciamos)
+        sfiMiIndicadorRefresh = (SwipeRefreshLayout) findViewById(R.id.sfiMiIndicadorRefresh);
+
+        lstMiLista = (ListView) findViewById(R.id.lstMiLista);
+
+        //le pasamos la lista de planetas definida
+        String[] planetas = getResources().getStringArray(R.array.planetas);
+
+        //se lo pasamos al adaptador
+        lstMiLista.setAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1, planetas));
+        //simple_list_item_1 se refeire al tipo de layout, o sea una sola linea
+        //un list view sencillo
+
+        //setOnRefreshListener(): cuando se haga gesto de refrescar, estara escuchando
+        sfiMiIndicadorRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refreshContent();
+            }
+        });
+    }
+
+    //Este metodo refrescara el contenido del list view
+    public void refreshContent(){
+        String[] planetas = getResources().getStringArray(R.array.planetas);
+
+        lstMiLista.setAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1, planetas));
+
+        sfiMiIndicadorRefresh.setRefreshing(false);//indica que ya se refresco la vuelta
     }
 
     public void agregarFav(){
